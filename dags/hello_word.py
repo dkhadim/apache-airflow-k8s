a1b31fc8d33097c34b3656c02b76eb4c66954d21
Khadim Diakhate <kdiakhate2412@gmail.com>
2024-05-23 18:30:32 +0200

    adding .gitignore, our first dag and values for setting up

---
+++ dags/hello_word.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+
+with DAG(
+        dag_id="hello_world",
+        start_date=datetime(2024, 5, 23),
+        schedule="0 * * * *",
+        tags=["hello"]
+    ) as dag:
+    
+    @task()
+    def say_hello():
+        print("Hello, ")
+        
+    @task()
+    def say_world():
+        print("World guys !")
+        
+    say_hello() >> say_world()    
